c3fae969d11b3b78280cebf41d5213f22b86dd9c
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-02-22 01:47:06 -0800

    Add files via upload

---
+++ C/Detach a three-digit integer (10 point(s)).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+int x,z=0,y=0;
+
+scanf("%d",&x);
+
+	z=x/100;
+	x=x%100;
+	printf("%d, ",z);
+	y=x/10;
+	printf("%d, ",y);
+	z=x%10;
+	printf("%d",z);
+
+
+
+return 0;
+}
